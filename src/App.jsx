56d31ad5ecfe5f82c5586c5aa00d18f30850f3f9
Sharma, Geet <gesharma@iu.edu>
2023-12-25 03:31:51 +0530

    test deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import solidLogo from './assets/solid.svg'
 import logo from './assets/C.png'
 import './App.css'
 import backdrop from './assets/tickeris.png'
+import dexs from './assets/dexscreener.png'
+import jup from './assets/jup.png'
+import twt from './assets/twitter.png'
+import tg from './assets/telegram.png'
 
 
 // Lock scroll
 document.body.style.overflow = 'hidden';
 
 // Unlock scroll
-document.body.style.overflow = 'visible';
+// document.body.style.overflow = 'visible';
 
 function App() {
   const [showLinks, setShowLinks] = createSignal(false);
@@ -30,17 +34,17 @@ function App() {
       </div>
       <h1>$seETHe</h1>
       <div class="logo-container">
-        <a href="#" class="logo logo-top-left"> {/* Position this logo at the top left */}
-          <img src="path_to_logo" alt="Logo 1" />
+        <a href="https://dexscreener.com/solana/dwwreqtpsewhuiy8dr53xwvnroam8uvejslenmcg31fc" target="_blank" class="logo logo-top-left"> {/* Position this logo at the top left */}
+          <img src={dexs} alt="DexScreener" />
         </a>
-        <a href="#" class="logo logo-top-right"> {/* Position this logo at the top right */}
-          <img src="path_to_logo" alt="Logo 2" />
+        <a href="https://jup.ag/swap/USDC-SEETHE_H838bbwxykb9pjfT9qpXhA3y1sFfNBsrmHk1c9gF7XH1" target="_blank" class="logo logo-top-right"> {/* Position this logo at the top right */}
+          <img src={jup} alt="Jupiter (jup.ag)" />
         </a>
-        <a href="#" class="logo logo-bottom-left"> {/* Position this logo at the bottom left */}
-          <img src="path_to_logo" alt="Logo 3" />
+        <a href="https://twitter.com/seetheonsolana" target="_blank" class="logo logo-bottom-left"> {/* Position this logo at the bottom left */}
+          <img src={twt} alt="Twitter" />
         </a>
-        <a href="#" class="logo logo-bottom-right"> {/* Position this logo at the bottom right */}
-          <img src="path_to_logo" alt="Logo 4" />
+        <a href="https://t.me/ZG8uFhLYR6x19bwKsPt17CqT2nRWtXcR" target="_blank" class="logo logo-bottom-right"> {/* Position this logo at the bottom right */}
+          <img src= {tg}alt="Telegram" />
         </a>
       </div>
 
