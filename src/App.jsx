e27483a44de8b8cc5e853eb154b4c3b19ba8af2a
cryptofelon <98140254+cryptofelon@users.noreply.github.com>
2023-12-25 15:35:34 -0500

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,12 @@ import dexs from './assets/dexscreener.png'
 import jup from './assets/jup.png'
 import twt from './assets/twitter.png'
 import tg from './assets/telegram.png'
-// import { Navigation } from './components/Navigation'
-
-
 
 
 function App() {
 
   return (
-
-
     <div class='main-container'>
-
       {/* Top nav */}
 
       <div class="logo-container z-10 ">
@@ -26,10 +20,7 @@ function App() {
         <a href={SocialURLConstants.jupiter} target="_blank" class="logo-img">
           <img src={jup} alt="Jupiter (jup.ag)" />
         </a>
-
       </div>
-
-
 
       {/* Seethe header image */}
       <div class=" w-full flex flex-col justify-center self-center mx-auto z-[-1] ">
@@ -39,9 +30,7 @@ function App() {
       </div>
 
       {/* Bottom Nav */}
-
       <div class="logo-container z-10 ">
-
         <a href={SocialURLConstants.twitter} target="_blank" class="logo-img">
           <img src={twt} alt="Twitter" />
         </a>
@@ -51,14 +40,11 @@ function App() {
       </div>
 
 
-      {/* <Navigation /> */}
-
-
+      <footer class='mt-[7%] italic text-black'>
+        You can't spell seethe without ETH
+      </footer>
 
     </div >
-
-
-
   )
 }
 
