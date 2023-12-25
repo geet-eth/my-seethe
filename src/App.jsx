b99c5c41a7e073458cd6acedb57eae739cef775a
cryptofelon <98140254+cryptofelon@users.noreply.github.com>
2023-12-25 15:28:26 -0500

    flexbox replace position absolute

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 import backdrop from './assets/tickeris.png'
-
-import { Navigation } from './components/Navigation'
+import { SocialURLConstants } from './Constants'
+import dexs from './assets/dexscreener.png'
+import jup from './assets/jup.png'
+import twt from './assets/twitter.png'
+import tg from './assets/telegram.png'
+// import { Navigation } from './components/Navigation'
 
 
 
@@ -9,31 +13,52 @@ import { Navigation } from './components/Navigation'
 function App() {
 
   return (
-    <>
 
 
-      <div class='main-container z-10'>
+    <div class='main-container'>
 
+      {/* Top nav */}
 
-        {/* <div class="absolute w-full flex flex-col justify-center self-center mx-auto z-[-1]"> */}
-        {/* <div class="absolute w-full flex flex-col justify-center self-center mx-auto z-[-1] mt-[0] sm:mt-[20rem]"> */}
-        <div class="absolute w-full flex flex-col justify-center self-center mx-auto z-[-1] mt-[0] sm:mt-[16rem]">
-          {/* <img src={backdrop} class=""></img> */}
-          <img src={backdrop} class="mx-auto "></img>
-          <h1 class='z-10'>$seETHe</h1>
-
-        </div>
-
-
-        <Navigation />
-
-
+      <div class="logo-container z-10 ">
+        <a href={SocialURLConstants.dexScreener} target="_blank" class="logo-img">
+          <img src={dexs} alt="DexScreener" />
+        </a>
+        <a href={SocialURLConstants.jupiter} target="_blank" class="logo-img">
+          <img src={jup} alt="Jupiter (jup.ag)" />
+        </a>
 
       </div>
 
 
 
-    </>
+      {/* Seethe header image */}
+      <div class=" w-full flex flex-col justify-center self-center mx-auto z-[-1] ">
+        <img src={backdrop} class="mx-auto "></img>
+        <h1 class='z-10'>$seETHe</h1>
+
+      </div>
+
+      {/* Bottom Nav */}
+
+      <div class="logo-container z-10 ">
+
+        <a href={SocialURLConstants.twitter} target="_blank" class="logo-img">
+          <img src={twt} alt="Twitter" />
+        </a>
+        <a href={SocialURLConstants.telegram} target="_blank" class="logo-img">
+          <img src={tg} alt="Telegram" />
+        </a>
+      </div>
+
+
+      {/* <Navigation /> */}
+
+
+
+    </div >
+
+
+
   )
 }
 
